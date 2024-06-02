1650885998fe65ab991cabc70c34775f876127ff
Mariana-Kyiv <149775691+Mariana-Kyiv@users.noreply.github.com>
2024-06-02 17:47:49 +0300

    added link on Git

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        <p>This project was made by Mariana Marynchenko and it's open-sourced on <a href="https://github.com/Mariana-Kyiv/weather-react-app-1" target='_blank' rel="noreferrer">GitHub</a></p>
+      </footer>
     </div>
   );
 }
