d329c035fb02874a7f067af58ec10b35d38bbc1c
Mariana-Kyiv <149775691+Mariana-Kyiv@users.noreply.github.com>
2024-06-02 18:26:55 +0300

    React Weather Project / HTML

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
+import Weather from './Weather';
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-      <h1>Weather App</h1>
+      <Weather />
       <footer>
         <p>This project was made by Mariana Marynchenko and it's open-sourced on <a href="https://github.com/Mariana-Kyiv/weather-react-app-1" target='_blank' rel="noreferrer">GitHub</a></p>
       </footer>
