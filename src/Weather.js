4430aa51ea878ac2116c13669cf0525a6a27c408
Mariana-Kyiv <149775691+Mariana-Kyiv@users.noreply.github.com>
2024-06-05 15:04:58 +0300

    added true data and spinner loader

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./Weather.css"
 import axios from "axios";
+import ClipLoader from "react-spinners/ClipLoader";
 
  export default function Weather (){
     const [ready, setReady] = useState(false);
@@ -34,36 +35,43 @@ import axios from "axios";
         <h1>{weatherData.city}</h1>
         <ul>
             <li>Sunday 18:00</li>
-            <li>{weatherData.description}</li>
+            <li className="text-capitalize">{weatherData.description}</li>
         </ul>
         <div className="row">
             <div className="col-6 d-flex align-items-start">
-                
-                <img src={weatherData.icon} alt={weatherData.description}></img>
+            <img src={weatherData.icon} alt={weatherData.description}></img>
            <span className="temperature">{Math.round(weatherData.temperature)}</span>
             <span className="unit">°C</span>
             </div>
           
             <div className="col-6">
                 <ul>
-                    <li>Visibility: {weatherData.visibility}%</li>
-                    <li>Humidity: {weatherData.humidity}%</li>
                     <li>Wind: {Math.round(weatherData.wind)} km/h</li>
+                    <li>Humidity: {weatherData.humidity}%</li>
+                    <li>Visibility: {(weatherData.visibility) / 100}%</li>
                 </ul>
             </div>
         </div>
-
        </div> 
     )
     }
     else {
-
 const apiKey = `197ef3a642b76eef90e131866f74a0a0`;
 let city =`Kyiv`;
 const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(handleResponse);
     return(
-        <p>Loading</p>
+        <div className="text-center">
+            <p>Loading...</p>
+      <ClipLoader
+        color="#0069D9"
+        loading={true}
+        cssOverride={1}
+        size={150}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+    </div>
     )
     }
   
