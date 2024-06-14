5f88c02b1ae1e4eda7e652182f70e3cec8dd503c
Mariana-Kyiv <149775691+Mariana-Kyiv@users.noreply.github.com>
2024-06-14 11:56:59 +0300

    added dayly forecast for week

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -15,9 +15,16 @@ export default function WeatherForecast(props){
         return (
       <div className="WeatherForecast">
         <div className="row">
-            <div className="col">
-               <ForecastDay data={forecastData[0]}/>
-            </div>
+            {forecastData.map( function (dailyForecast, index) {
+                   if (index <7) {
+                return (
+                        <div className="col" key={index}>
+                        <ForecastDay data={dailyForecast} />
+                        </div>
+                );
+                   } else return null
+            })}
+            
         </div>
       </div>
     );
